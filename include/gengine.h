1b214ed46a80ff10b2cbb1b2f246924d654d09d0
Gabriele Cocchieri <gabriele.cocchieri@gmail.com>
2026-07-28 20:45:03 +0200

    Initial commit

---
+++ include/gengine.h
@@ -0,0 +1,10 @@
+#ifndef GENGINE_H_
+#define GENGINE_H_
+
+/*
+ *      Gabro's Game Engine
+ */
+
+
+
+#endif
---
+++ test/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <gengine.h>
+
+int main()
+{
+	printf("All done.\n");
+    return 0;
+}
